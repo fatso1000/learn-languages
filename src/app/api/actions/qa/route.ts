2bc5219a628c34e2fdd84d9f1f41f1f92a08e45c
chocolule <fideos.12.11@gmail.com>
2024-01-02 13:49:07 -0300

    saving commit

---
+++ src/app/api/actions/qa/route.ts
@@ -0,0 +1,33 @@
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "src/app/config/db";
+import { onThrowError } from "../../apiService";
+
+export async function POST(req: NextRequest) {
+  try {
+    let message = "Historical Saved successfully";
+    const { user_id, content_id } = await req.json();
+
+    const isExisting = await prisma.userRank.findFirst({
+      where: {
+        user_id,
+      },
+    });
+
+    if (!isExisting) {
+      await prisma.userRank.create({
+        data: { user_id, content_id },
+      });
+    } else {
+      const current_date = new Date();
+      await prisma.historical.update({
+        where: { id: isExisting.id },
+        data: { last_watched: current_date },
+      });
+      message = "Historical modified successfully.";
+    }
+
+    return NextResponse.json({ message });
+  } catch (error) {
+    return onThrowError(error);
+  }
+}
--- src/components/Reading/QA/index.tsx
+++ src/components/Reading/QA/index.tsx
@@ -5,6 +5,7 @@ import { ErrorIconCircle, SuccessIconCircle } from "src/components/Icons";
 import { IQuestionAndAnswer } from "src/types";
 // @ts-expect-error
 import confetti from "canvas-confetti";
+import { toast } from "react-toastify";
 
 export async function generateMetadata(props: any): Promise<Metadata> {
   return {
@@ -18,11 +19,19 @@ export default function QA(props: { values: IQuestionAndAnswer[] }) {
 
   const { values } = props;
 
+  // section = 52, perder = -13
+
   useEffect(() => {
     setQuestionAndAnswers(
       values.map((v) => ({
         ...v,
-        options: [...v.options.map((x) => ({ title: x, status: "default" }))],
+        options: [
+          ...v.options.map((x) => ({
+            title: x,
+            status: "default",
+            points: 0,
+          })),
+        ],
       }))
     );
   }, []);
@@ -47,6 +56,7 @@ export default function QA(props: { values: IQuestionAndAnswer[] }) {
               ? {
                   title: x.title,
                   status: "incorrect",
+                  points: -13,
                 }
               : x;
           }),
@@ -61,8 +71,13 @@ export default function QA(props: { values: IQuestionAndAnswer[] }) {
               ? {
                   title: x.title,
                   status: "correct",
+                  points: 13,
                 }
-              : { title: x.title, status: "incorrect" }
+              : {
+                  title: x.title,
+                  status: "completed",
+                  points: x.points === null ? 0 : x.points,
+                }
           ),
         ],
       };
@@ -73,6 +88,27 @@ export default function QA(props: { values: IQuestionAndAnswer[] }) {
     }
     setQuestionAndAnswers(response);
   };
+
+  useEffect(() => {
+    if (questionAndAnswers.length === 0) return;
+    const isQACompleted = questionAndAnswers.every(
+      (x: any) =>
+        x.options.findIndex((v: any) => v.status === "completed") !== -1
+    );
+    if (isQACompleted) {
+      const points = questionAndAnswers.reduce((acc, answer) => {
+        return (
+          acc +
+          answer.options.reduce((rd: number, opt: any) => {
+            return rd + opt.points;
+          }, 0)
+        );
+      }, 0);
+      toast("Exercises completed!", {
+        type: "success",
+      });
+    }
+  }, [questionAndAnswers]);
 
   return (
     <div className="mt-6 flex flex-col gap-y-5">
@@ -91,7 +127,9 @@ export default function QA(props: { values: IQuestionAndAnswer[] }) {
                     id={x.title}
                     onClick={() => onAnswerClick(x, v.correct_answer, i)}
                     className={`btn ${
-                      x.status !== "default"
+                      x.status === "completed"
+                        ? "btn-disabled text-neutral-50"
+                        : x.status !== "default"
                         ? x.status === "correct"
                           ? "btn-success text-neutral-50"
                           : "btn-error text-neutral-50 opacity-70"
