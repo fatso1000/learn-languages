810ad2ab573dbffef480b0508a418dbde0a00ca3
chocolule <fideos.12.11@gmail.com>
2024-02-04 17:27:28 -0300

    fix and improve code

--- src/components/Section/LevelIcon/index.tsx
+++ src/components/Section/LevelIcon/index.tsx
@@ -1,71 +1,15 @@
-function CompletedIcon() {
-  return (
-    <h4 className="text-3xl font-extrabold ">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 24 24"
-        fill="currentColor"
-        className="w-10 h-10"
-      >
-        <path
-          fillRule="evenodd"
-          d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z"
-          clipRule="evenodd"
-        />
-      </svg>
-    </h4>
-  );
-}
-
-function BlockedIcon() {
-  return (
-    <h4 className="text-3xl font-extrabold">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 24 24"
-        fill="currentColor"
-        className="w-10 h-10"
-      >
-        <path
-          fillRule="evenodd"
-          d="M12 1.5a5.25 5.25 0 0 0-5.25 5.25v3a3 3 0 0 0-3 3v6.75a3 3 0 0 0 3 3h10.5a3 3 0 0 0 3-3v-6.75a3 3 0 0 0-3-3v-3c0-2.9-2.35-5.25-5.25-5.25Zm3.75 8.25v-3a3.75 3.75 0 1 0-7.5 0v3h7.5Z"
-          clipRule="evenodd"
-        />
-      </svg>
-    </h4>
-  );
-}
-
-function StudyingIcon() {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      fill="currentColor"
-      className="w-10 h-10"
-    >
-      <path
-        fillRule="evenodd"
-        d="M4.5 5.653c0-1.427 1.529-2.33 2.779-1.643l11.54 6.347c1.295.712 1.295 2.573 0 3.286L7.28 19.99c-1.25.687-2.779-.217-2.779-1.643V5.653Z"
-        clipRule="evenodd"
-      />
-    </svg>
-  );
-}
-
-interface LevelIconProps {
-  state: "completed" | "studying" | "first-blocked" | "blocked";
-}
+import { LockIcon, PlayIcon, StarIcon } from "src/components/Icons";
+import { LevelIconProps } from "src/types";
 
 export default function LevelIcon({ state }: LevelIconProps) {
   return (
-    <div className={`flex justify-center items-center`}>
+    <div className="flex justify-center items-center">
       {state === "completed" ? (
-        <CompletedIcon />
+        <StarIcon />
       ) : state === "studying" ? (
-        <StudyingIcon />
+        <PlayIcon />
       ) : (
-        <BlockedIcon />
+        <LockIcon />
       )}
     </div>
   );
