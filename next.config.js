1060e2d958905907e414bcb99e6e56fc20a535be
fatso1000 <agustinbenitez81@gmail.com>
2024-08-01 23:03:53 -0300

    Update cors

--- next.config.js
+++ next.config.js
@@ -11,6 +11,26 @@ const nextConfig = {
       "@react-email/render",
     ],
   },
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,DELETE,PATCH,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
+  },
   images: {
     domains: ["drive.google.com", "www.katywang.co.uk"],
   },
